31ae9220f540480462edd46b1c82e9deaa98ed3b
Daniel Beßler <danielb@uni-bremen.de>
2021-06-04 21:16:11 +0200

    refactor

--- src/lang/mongolog/mongolog.pl
+++ src/lang/mongolog/mongolog.pl
@@ -19,7 +19,7 @@
 	    [ rdf_meta/1, rdf_global_term/2 ]).
 :- use_module(library('db/mongo/client')).
 
-:- use_module('stages/aggregation').
+:- use_module('stages/aggregation', [ aggregate/4 ]).
 :- use_module('stages/bulk_operation', [ bulk_operation/1 ]).
 
 %% set of registered query commands.
@@ -170,141 +170,9 @@ mongolog_call(Goal, Context) :-
 	append(Vars1, GlobalVars, Vars2),
 	list_to_set(Vars2,Vars3),
 	% run the pipeline
-	query_1(Coll, Doc, Vars3).
-
-query_1(Coll, Pipeline, Vars) :-
-	% get DB for cursor creation. use collection with just a
-	% single document as starting point.
-	%mng_one_db(DB, Coll),
-	mng_db_name(DB),
-	% run the query
-	setup_call_cleanup(
-		% setup: create a query cursor
-		mng_cursor_create(DB, Coll, Cursor),
-		% call: find matching document
-		(	mng_cursor_aggregate(Cursor, ['pipeline',array(Pipeline)]),
-			query_2(Cursor, Vars)
-		),
-		% cleanup: destroy cursor again
-		mng_cursor_destroy(Cursor)
-	).
-
-%%
-query_2(Cursor, Vars) :-
-	mng_cursor_materialize(Cursor, Result),
-	unify_(Result, Vars),
+	% TODO: split goal at assert's such that they are available in the rest of the query
+	aggregate(Coll, Doc, Vars3, Result),
 	bulk_operation(Result).
-
-%%
-% unify variables.
-%
-unify_(Result, Vars) :-
-	unify_0(Result, Vars, Vars).
-
-unify_0(_, _, []) :- !.
-unify_0(Doc, Vars, [[VarKey, Term]|Xs]) :-
-	% it is ignored here if some variables referred
-	% to are not properly grounded.
-	% this can happen e.g. in expressions such as (A=a;B=b)
-	% where the first solution has grounded A but not B.
-	(	ground(Term)
-	->	once(unify_grounded(Doc, [VarKey, Term]))
-	;	ignore(unify_1(Doc, Vars, [VarKey, Term]))
-	),
-	unify_0(Doc, Vars, Xs).
-
-unify_grounded(Doc, [VarKey, Term]) :-
-	% variable was unified in pragma command
-	% make sure it did not get another grounding in the query
-	mng_get_dict(VarKey, Doc, TypedValue),
-	!,
-	mng_strip_type(TypedValue, _, Value),
-	% ignore if value in the document is a variable
-	(	Value=_{ type: string(var), value: _ }
-	% try to unify
-	;	Term=Value
-	% special case for number comparison, e.g. `5.0 =:= 5`
-	;	(	number(Term),
-			number(Value),
-			Term=:=Value
-		)
-	).
-unify_grounded(_, [_, _]) :- !.
-
-unify_1(_, _, ['_id', _]).
-
-unify_1(Doc, Vars, [VarKey, Val]) :-
-	mng_get_dict(VarKey, Doc, TypedValue),
-	unify_2(TypedValue, Vars, Val).
-
-%%
-unify_2(string(In), _Vars, X) :-
-	% handle case that variable is wrapped in term/1.
-	% if this is the case then convert input string to term.
-	nonvar(X),
-	X=term(Out),!,
-	atom_to_term_(In, Out).
-
-unify_2(array(In), Vars, Out) :-
-	% a variable was instantiated to a list
-	!,
-	unify_array(In, Vars, Out).
-
-unify_2([K-V|Rest], Vars, Out) :-
-	!,
-	dict_pairs(Dict,_,[K-V|Rest]),
-	unify_2(Dict, Vars, Out).
-
-unify_2(_{
-		type: string(var),
-		value: string(VarKey)
-	}, Vars, Out) :-
-	% a variable was not instantiated
-	!,
-	memberchk([VarKey, VarVal], Vars),
-	Out = VarVal. 
-
-unify_2(_{
-		type: string(compound),
-		value: Val
-	}, Vars, Out) :-
-	% a variable was instantiated to a compound term
-	!,
-	unify_2(Val, Vars, Out).
-
-unify_2(_{
-		functor: string(Functor),
-		args: Args
-	}, Vars, Out) :-
-	!,
-	unify_2(Args, Vars, Args0),
-	Out =.. [Functor|Args0].
-
-unify_2(TypedValue, _, Value) :-
-	% a variable was instantiated to an atomic value
-	mng_strip_type(TypedValue, _, Value).
-
-%%
-unify_array([], _, []) :- !.
-unify_array([X|Xs], Vars, [Y|Ys]) :-
-	unify_2(X, Vars, Y),
-	unify_array(Xs, Vars, Ys).
-
-%%
-atom_to_term_(Atom, Term) :-
-	% try converting atom stored in DB to a Prolog term
-	catch(term_to_atom(Term,Atom), _, fail),
-	!.
-
-atom_to_term_(Atom, Term) :-
-	% vectors maybe stored space-separated.
-	% @deprecated
-	atomic_list_concat(Elems, ' ', Atom),
-	maplist(atom_number, Elems, Term),
-	!.
-
-atom_to_term_(Atom, _) :-
-	throw(error(conversion_error(atom_to_term(Atom)))).
 
 
 %% mongolog_compile(+Term, -CompilerOutput, -Variables, +Context) is semidet.
--- src/lang/mongolog/stages/aggregation.pl
+++ src/lang/mongolog/stages/aggregation.pl
@@ -1,6 +1,136 @@
 :- module(mongolog_aggregation,
-	[ aggregate/0
+	[ aggregate/4
 	]).
 
-aggregate :-
-	true.
+:- use_module(library('db/mongo/client')).
+
+%%
+%
+aggregate(Coll, Pipeline, Vars, Result) :-
+	% get DB for cursor creation. use collection with just a
+	% single document as starting point.
+	%mng_one_db(DB, Coll),
+	mng_db_name(DB),
+	% run the query
+	setup_call_cleanup(
+		% setup: create a query cursor
+		mng_cursor_create(DB, Coll, Cursor),
+		% call: find matching document
+		(	mng_cursor_aggregate(Cursor, ['pipeline',array(Pipeline)]),
+			mng_cursor_materialize(Cursor, Result),
+			unify_(Result, Vars)
+		),
+		% cleanup: destroy cursor again
+		mng_cursor_destroy(Cursor)
+	).
+
+%%
+% unify variables.
+%
+unify_(Result, Vars) :-
+	unify_0(Result, Vars, Vars).
+
+unify_0(_, _, []) :- !.
+unify_0(Doc, Vars, [[VarKey, Term]|Xs]) :-
+	% it is ignored here if some variables referred
+	% to are not properly grounded.
+	% this can happen e.g. in expressions such as (A=a;B=b)
+	% where the first solution has grounded A but not B.
+	(	ground(Term)
+	->	once(unify_grounded(Doc, [VarKey, Term]))
+	;	ignore(unify_1(Doc, Vars, [VarKey, Term]))
+	),
+	unify_0(Doc, Vars, Xs).
+
+unify_grounded(Doc, [VarKey, Term]) :-
+	% variable was unified in pragma command
+	% make sure it did not get another grounding in the query
+	mng_get_dict(VarKey, Doc, TypedValue),
+	!,
+	mng_strip_type(TypedValue, _, Value),
+	% ignore if value in the document is a variable
+	(	Value=_{ type: string(var), value: _ }
+	% try to unify
+	;	Term=Value
+	% special case for number comparison, e.g. `5.0 =:= 5`
+	;	(	number(Term),
+			number(Value),
+			Term=:=Value
+		)
+	).
+unify_grounded(_, [_, _]) :- !.
+
+unify_1(_, _, ['_id', _]).
+
+unify_1(Doc, Vars, [VarKey, Val]) :-
+	mng_get_dict(VarKey, Doc, TypedValue),
+	unify_2(TypedValue, Vars, Val).
+
+%%
+unify_2(string(In), _Vars, X) :-
+	% handle case that variable is wrapped in term/1.
+	% if this is the case then convert input string to term.
+	nonvar(X),
+	X=term(Out),!,
+	atom_to_term_(In, Out).
+
+unify_2(array(In), Vars, Out) :-
+	% a variable was instantiated to a list
+	!,
+	unify_array(In, Vars, Out).
+
+unify_2([K-V|Rest], Vars, Out) :-
+	!,
+	dict_pairs(Dict,_,[K-V|Rest]),
+	unify_2(Dict, Vars, Out).
+
+unify_2(_{
+		type: string(var),
+		value: string(VarKey)
+	}, Vars, Out) :-
+	% a variable was not instantiated
+	!,
+	memberchk([VarKey, VarVal], Vars),
+	Out = VarVal. 
+
+unify_2(_{
+		type: string(compound),
+		value: Val
+	}, Vars, Out) :-
+	% a variable was instantiated to a compound term
+	!,
+	unify_2(Val, Vars, Out).
+
+unify_2(_{
+		functor: string(Functor),
+		args: Args
+	}, Vars, Out) :-
+	!,
+	unify_2(Args, Vars, Args0),
+	Out =.. [Functor|Args0].
+
+unify_2(TypedValue, _, Value) :-
+	% a variable was instantiated to an atomic value
+	mng_strip_type(TypedValue, _, Value).
+
+%%
+unify_array([], _, []) :- !.
+unify_array([X|Xs], Vars, [Y|Ys]) :-
+	unify_2(X, Vars, Y),
+	unify_array(Xs, Vars, Ys).
+
+%%
+atom_to_term_(Atom, Term) :-
+	% try converting atom stored in DB to a Prolog term
+	catch(term_to_atom(Term,Atom), _, fail),
+	!.
+
+atom_to_term_(Atom, Term) :-
+	% vectors maybe stored space-separated.
+	% @deprecated
+	atomic_list_concat(Elems, ' ', Atom),
+	maplist(atom_number, Elems, Term),
+	!.
+
+atom_to_term_(Atom, _) :-
+	throw(error(conversion_error(atom_to_term(Atom)))).
